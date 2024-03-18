e345181c4af0c6230bfe21fbaf5a495db0166659
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-03-18 22:00:11 +0100

    appointment changes

--- src/Ekid.Domain/Calendar/Appointment.cs
+++ src/Ekid.Domain/Calendar/Appointment.cs
@@ -2,12 +2,11 @@ namespace Ekid.Domain.Calendar;
 
 public class Appointment
 {
-    public Appointment(Guid attendeeId, Guid activityId, Guid employeeId, WeekDay weekDay, DateTime startTime, DateTime endTime)
+    public Appointment(Guid attendeeId, Guid activityId, Guid employeeId, DateTime startTime, DateTime endTime)
     {
         AttendeeId = attendeeId;
         ActivityId = activityId;
         EmployeeId = employeeId;
-        WeekDay = weekDay;
         StartTime = startTime;
         EndTime = endTime;
     }
@@ -15,7 +14,6 @@ public class Appointment
     public Guid AttendeeId { get; }
     public Guid ActivityId { get; }
     public Guid EmployeeId { get; }
-    public WeekDay WeekDay { get; }
     public DateTime StartTime { get; }
     public DateTime EndTime { get; }
 }
--- src/Ekid.Domain/Calendar/Calendar.cs
+++ src/Ekid.Domain/Calendar/Calendar.cs
@@ -4,28 +4,30 @@ public class Calendar
 {
     private readonly List<Appointment> _appointments = new List<Appointment>();
 
-    public void ScheduleAppointment(Guid attendeeId, Guid employeeId, Guid activityId, WeekDay day, DateTime startTime, DateTime endTime)
+    public void ScheduleAppointment(Guid attendeeId, Guid employeeId, Guid activityId, DateTime startTime, DateTime endTime)
     {
-        if (IsSlotAvailable(startTime, endTime, activityId, day))
+        if (IsSlotAvailable(startTime, endTime, activityId))
         {
             _appointments.Add(new Appointment(
                 attendeeId: attendeeId,
                 activityId: activityId,
                 employeeId: employeeId,
-                weekDay: day,
                 startTime: startTime,
                 endTime: endTime));
         }
     }
 
-    public void GetAppointments(Guid activityId, Guid employeeId, DateTime startDate, DateTime endDate)
+    public IReadOnlyCollection<Appointment> GetAppointments(Guid activityId, Guid employeeId, DateTime startDate, DateTime endDate)
     {
-        
+        return _appointments.Where(x => x.ActivityId == activityId
+                                         && x.EmployeeId == employeeId
+                                         && x.StartTime >= startDate
+                                         && x.EndTime <= endDate).ToList();
     }
 
-    private bool IsSlotAvailable(DateTime startTime, DateTime endTime, Guid activityId, WeekDay day) 
+    private bool IsSlotAvailable(DateTime startTime, DateTime endTime, Guid activityId) 
     {
-        foreach (var appointment in _appointments.Where(x => x.ActivityId == activityId && x.WeekDay == day)) 
+        foreach (var appointment in _appointments.Where(x => x.ActivityId == activityId)) 
         {
             if ((startTime >= appointment.StartTime && startTime < appointment.EndTime) ||
                 (endTime > appointment.StartTime && endTime <= appointment.EndTime) ||
