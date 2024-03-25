6749f4a8b91467440d82c4df537a134d2ea2de35
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-03-25 20:38:46 +0100

    added training program

---
+++ src/Ekid.Domain/ElectronicJournal/Individual/TrainingProgram.cs
@@ -0,0 +1,8 @@
+namespace Ekid.Domain.ElectronicJournal.Individual;
+
+public class TrainingProgram
+{
+    public Guid Id { get; }
+    public Guid JournalId { get; }
+    public string Description { get; }
+}
