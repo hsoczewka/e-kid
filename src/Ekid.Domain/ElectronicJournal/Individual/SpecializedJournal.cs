550e61a6f6a0e432fa4f31c8e3ae96374dcb8689
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-03-29 22:03:35 +0100

    extend specialized journal entity

--- src/Ekid.Domain/ElectronicJournal/Individual/SpecializedJournal.cs
+++ src/Ekid.Domain/ElectronicJournal/Individual/SpecializedJournal.cs
@@ -2,16 +2,20 @@ namespace Ekid.Domain.ElectronicJournal.Individual;
 
 public class SpecializedJournal
 {
-    public SpecializedJournal(Guid attendeeId, string description, string opinionNumber, string diagnosisResult)
+    public SpecializedJournal(Guid id, Guid attendeeId, string description, string opinionNumber, string diagnosisResult, List<Guid> employeesIds)
     {
+        Id = id;
         AttendeeId = attendeeId;
         Description = description;
         OpinionNumber = opinionNumber;
         DiagnosisResult = diagnosisResult;
+        EmployeesIds = employeesIds;
     }
 
+    public Guid Id;
     public Guid AttendeeId { get; }
     public string Description { get; }
     public string OpinionNumber { get; }
     public string DiagnosisResult { get; }
+    public List<Guid> EmployeesIds { get; }
 }
