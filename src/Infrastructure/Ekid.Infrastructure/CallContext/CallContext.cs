9009e03400d5cc5ee608d7175209f5258bccdc80
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-03-30 20:35:16 +0100

    extend call context with tenat id

--- src/Infrastructure/Ekid.Infrastructure/CallContext/CallContext.cs
+++ src/Infrastructure/Ekid.Infrastructure/CallContext/CallContext.cs
@@ -2,12 +2,14 @@ namespace Ekid.Infrastructure.CallContext;
 
 public class CallContext
 {
-    public CallContext(Guid correlationId, Guid userId)
+    public CallContext(Guid tenantId, Guid correlationId, Guid userId)
     {
+        TenantId = tenantId;
         CorrelationId = correlationId;
         UserId = userId;
     }
 
+    public Guid TenantId { get; }
     public Guid CorrelationId { get; }
     public Guid UserId { get; }
 }
