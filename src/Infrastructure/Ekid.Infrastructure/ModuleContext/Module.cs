9801fde9efbf1ce3bddcb667794da1226b0dd3f1
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-04-02 08:03:57 +0200

    added module definition

---
+++ src/Infrastructure/Ekid.Infrastructure/ModuleContext/Module.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ekid.Infrastructure.ModuleContext;
+
+public abstract class Module
+{
+    public abstract string RoutePrefix { get; }
+    public abstract void AddModuleComponents(IServiceCollection services, IConfiguration configuration);
+    public abstract void MapEndpoints(IEndpointRouteBuilder endpoints);
+}
