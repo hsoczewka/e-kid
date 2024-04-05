48bdfa0d8e4907f8328bf5e53f7f46e764c12616
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-04-05 22:16:11 +0200

    extend activity type

--- src/ResourcesManagement/Ekid.ResourcesManagement/Activities/ActivityType.cs
+++ src/ResourcesManagement/Ekid.ResourcesManagement/Activities/ActivityType.cs
@@ -6,4 +6,6 @@ public record ActivityType(string Value)
     public static ActivityType Lesson => new ActivityType(nameof(Lesson));
     public static ActivityType Consultation => new ActivityType(nameof(Consultation));
     public static ActivityType Diagnosis => new ActivityType(nameof(Diagnosis));
+    public static ActivityType SpeechTherapy => new ActivityType(nameof(SpeechTherapy));
+    public static ActivityType SensoryIntegration => new ActivityType(nameof(SensoryIntegration));
 }
