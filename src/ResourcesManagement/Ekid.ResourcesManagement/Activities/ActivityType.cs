9828dd4a7b459582e02b0d635c8e1657f6ad88b6
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-04-06 10:01:23 +0200

    changed activity type

--- src/ResourcesManagement/Ekid.ResourcesManagement/Activities/ActivityType.cs
+++ src/ResourcesManagement/Ekid.ResourcesManagement/Activities/ActivityType.cs
@@ -1,11 +1,14 @@
 namespace Ekid.ResourcesManagement.Activities;
 
-public record ActivityType(string Value)
+public enum ActivityType
 {
-    public static ActivityType Therapy => new ActivityType(nameof(Therapy));
-    public static ActivityType Lesson => new ActivityType(nameof(Lesson));
-    public static ActivityType Consultation => new ActivityType(nameof(Consultation));
-    public static ActivityType Diagnosis => new ActivityType(nameof(Diagnosis));
-    public static ActivityType SpeechTherapy => new ActivityType(nameof(SpeechTherapy));
-    public static ActivityType SensoryIntegration => new ActivityType(nameof(SensoryIntegration));
+    Therapy = 1,
+    Lesson,
+    Consultation,
+    Diagnosis,
+    SpeechTherapy,
+    SensoryIntegration,
+    Massage,
+    PsychologicalTherapy,
+    PsychologicalDiagnosis
 }
