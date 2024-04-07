806b3765fc7248a7c36ae232278b17f5cc23aa73
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-04-07 11:16:50 +0200

    activity price model

---
+++ src/ResourcesManagement/Ekid.ResourcesManagement/Activities/ActivityPrice.cs
@@ -0,0 +1,21 @@
+using Ekid.Infrastructure.Primitives;
+
+namespace Ekid.ResourcesManagement.Activities;
+
+public class ActivityPrice
+{
+    public ActivityPrice(Guid id, Guid activityId, Money price, DateTime validFrom, DateTime? validTo)
+    {
+        Id = id;
+        ActivityId = activityId;
+        Price = price;
+        ValidFrom = validFrom;
+        ValidTo = validTo;
+    }
+
+    public Guid Id { get; set; }
+    public Guid ActivityId { get; set; }
+    public Money Price { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime? ValidTo { get; set; }
+}
