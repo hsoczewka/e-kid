cc0ea1b02618bffed8a709997bfd74ac7d68cb7d
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-04-19 21:51:09 +0200

    looking for better approach

--- src/Ekid.Domain/Calendar/EmployeeAvailability.cs
+++ src/Ekid.Domain/Calendar/EmployeeAvailability.cs
@@ -7,3 +7,8 @@ public class EmployeeAvailability
     public Guid EmployeeId { get; }
     public List<TimeSlot> TimeSlots { get; }
 }
+
+//employee availability plan
+//plan can be created automatically for given period: week/month
+//automatically created plan should be in draft status waiting for employee approval
+//after plan is approved can be used for appointment activities
