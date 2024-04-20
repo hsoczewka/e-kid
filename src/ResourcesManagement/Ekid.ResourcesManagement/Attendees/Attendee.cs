82fd7a77868dbf60700716c9480fe7523195d0ac
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-04-20 22:24:44 +0200

    attendee changes

--- src/ResourcesManagement/Ekid.ResourcesManagement/Attendees/Attendee.cs
+++ src/ResourcesManagement/Ekid.ResourcesManagement/Attendees/Attendee.cs
@@ -4,15 +4,16 @@ namespace Ekid.ResourcesManagement.Attendees;
 
 public class Attendee
 {
-    public Attendee(Guid id, AttendeeDetails details, IEnumerable<AttendeeActivity> activities)
+    public Attendee(Guid id, Guid contactId, AttendeeDetails details, IEnumerable<AttendeeActivity> activities)
     {
         Id = id;
         Details = details;
         Activities = activities;
+        ContactId = contactId;
     }
 
     public Guid Id { get; }
-    //TODO ContactId => ContactDetails
+    public Guid ContactId { get; }
     public AttendeeDetails Details { get; }
     public IEnumerable<AttendeeActivity> Activities { get; }
 }
