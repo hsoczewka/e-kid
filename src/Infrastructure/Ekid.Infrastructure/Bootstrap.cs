e43d9f842869ad95da97a41cb1b50b97e69e27e2
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-05-07 21:58:32 +0200

    registration extensions for dispatcher

---
+++ src/Infrastructure/Ekid.Infrastructure/Bootstrap.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using Ekid.Infrastructure.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ekid.Infrastructure;
+
+public static class Bootstrap
+{
+    public static void AddInfrastructure(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+    {
+        services.AddCommandQueryDispatcherWithHandlers(assemblies);
+    }
+}
