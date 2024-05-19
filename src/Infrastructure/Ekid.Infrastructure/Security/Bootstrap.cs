9801ad64dc94dbf0b6396992547224a5c1f674a6
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-05-19 08:03:11 +0200

    permission registration function

--- src/Infrastructure/Ekid.Infrastructure/Security/Bootstrap.cs
+++ src/Infrastructure/Ekid.Infrastructure/Security/Bootstrap.cs
@@ -11,6 +11,18 @@ public static class Bootstrap
 
     public static void RegisterAllPermissions(this IServiceProvider serviceProvider)
     {
-        //TODO scan all assemblies for IPermissionRegistry implementation and add found items to the permissions container. 
+        //TODO scan all assemblies for IPermissionRegistry implementation and add found items to the permissions container.
+        var registries = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes())
+            .Where(type => typeof(IPermissionRegistry).IsAssignableFrom(type))
+            .Where(type => !type.IsAbstract)
+            .Where(type => type.IsClass)
+            .ToList();
+
+        foreach (var registry in registries)
+        {
+            var instance = (IPermissionRegistry)Activator.CreateInstance(registry)!;
+            instance.ConfigurePermissions();
+        }
+        
     }
 }
