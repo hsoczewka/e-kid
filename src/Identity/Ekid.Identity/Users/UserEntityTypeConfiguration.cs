ef69ca4181e0128781538ab7f0769acc340b3b54
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-05-24 08:21:50 +0200

    user entity type configuration

--- src/Identity/Ekid.Identity/Users/UserEntityTypeConfiguration.cs
+++ src/Identity/Ekid.Identity/Users/UserEntityTypeConfiguration.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Ekid.Identity.Users;
 
-public class UserEntityTypeConfiguration
+public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
 {
-    //TODO
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Email).IsRequired();
+        builder.Property(x => x.Login).IsRequired();
+        builder.Property(x => x.FirstName).IsRequired();
+        builder.Property(x => x.LastName).IsRequired();
+        builder.Property(x => x.Password).IsRequired();
+        builder.Property(x => x.Role).IsRequired()
+            .HasConversion(x => x.Value, x => new UserRole(x));
+    }
 }
