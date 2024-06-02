7bdd5152552e477044faf0383385dc66ca22a59d
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-06-02 12:11:55 +0200

    db migrations

---
+++ src/Identity/Ekid.Identity/Migrations/20240602101014_UsersEntitiesChanges.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ekid.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersEntitiesChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "assigned_permissions",
+                schema: "identity");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "identity");
+
+            migrationBuilder.AddColumn<ISet<Guid>>(
+                name: "Permissions",
+                schema: "identity",
+                table: "user_accounts",
+                type: "jsonb",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Permissions",
+                schema: "identity",
+                table: "user_accounts");
+
+            migrationBuilder.CreateTable(
+                name: "assigned_permissions",
+                schema: "identity",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Permissions = table.Column<ICollection<Guid>>(type: "jsonb", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_assigned_permissions", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "identity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+    }
+}
