08aeaa8d07fbf4d2c357c92d7416bf48e50514fd
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-06-10 08:19:15 +0200

    applied migration

--- src/Identity/Ekid.Identity/Migrations/20240610061637_UsersTablesUpdate.Designer.cs
+++ src/Identity/Ekid.Identity/Migrations/20240610061637_UsersTablesUpdate.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Ekid.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Ekid.Identity.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240610061637_UsersTablesUpdate")]
+    partial class UsersTablesUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Identity/Ekid.Identity/Migrations/20240610061637_UsersTablesUpdate.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ekid.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersTablesUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                schema: "identity",
+                table: "user_credentials",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "identity",
+                table: "user_credentials",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(320)",
+                oldMaxLength: 320);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "identity",
+                table: "user_accounts",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(320)",
+                oldMaxLength: 320);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_credentials_Email",
+                schema: "identity",
+                table: "user_credentials",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_credentials_Login",
+                schema: "identity",
+                table: "user_credentials",
+                column: "Login",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_accounts_Email",
+                schema: "identity",
+                table: "user_accounts",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_user_credentials_Email",
+                schema: "identity",
+                table: "user_credentials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_credentials_Login",
+                schema: "identity",
+                table: "user_credentials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_accounts_Email",
+                schema: "identity",
+                table: "user_accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                schema: "identity",
+                table: "user_credentials",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "identity",
+                table: "user_credentials",
+                type: "character varying(320)",
+                maxLength: 320,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                schema: "identity",
+                table: "user_accounts",
+                type: "character varying(320)",
+                maxLength: 320,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
