09c20ad60170d3f37f152ba97c28ec15e9705e29
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-06-10 07:58:28 +0200

    added user specific value objects

---
+++ src/Identity/Ekid.Identity/Users/Password.cs
@@ -0,0 +1,24 @@
+using Ekid.Identity.Users.Exceptions;
+
+namespace Ekid.Identity.Users;
+
+public sealed record Password
+{
+    public string Value { get; }
+        
+    public Password(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || value.Length is > 200 or < 6)
+        {
+            throw new InvalidPasswordException();
+        }
+            
+        Value = value;
+    }
+
+    public static implicit operator Password(string value) => new(value);
+
+    public static implicit operator string(Password value) => value?.Value;
+
+    public override string ToString() => Value;
+}
