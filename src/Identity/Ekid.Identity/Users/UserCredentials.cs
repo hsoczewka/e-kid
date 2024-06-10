4960b600c67386dc5a6d8ec9d982aafbad4c3eb4
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-06-10 08:07:36 +0200

    user account and user credentials tables update

--- src/Identity/Ekid.Identity/Users/UserCredentials.cs
+++ src/Identity/Ekid.Identity/Users/UserCredentials.cs
@@ -3,12 +3,12 @@ namespace Ekid.Identity.Users;
 public class UserCredentials
 {
     public UserId Id { get; private set; }
-    public string Login { get; private set; }
-    public string Password { get; private set; }
-    public string Email { get; private set; }
+    public Login Login { get; private set; }
+    public Password Password { get; private set; }
+    public Email Email { get; private set; }
     private int _version;
     
-    public UserCredentials(UserId id, string login, string password, string email)
+    public UserCredentials(UserId id, Login login, Password password, Email email)
     {
         Id = id;
         Login = login;
