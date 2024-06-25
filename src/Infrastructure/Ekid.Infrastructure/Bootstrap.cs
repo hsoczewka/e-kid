9690da6fd455295fe94fc545c8c3ded928061e04
Hubert Soczewka <hubert.soczewka@gmail.com>
2024-06-25 09:24:11 +0200

    auth and infrastructure changes

--- src/Infrastructure/Ekid.Infrastructure/Bootstrap.cs
+++ src/Infrastructure/Ekid.Infrastructure/Bootstrap.cs
@@ -1,17 +1,44 @@
 using Ekid.Infrastructure.AppInitialization;
+using Ekid.Infrastructure.Auth;
+using Ekid.Infrastructure.ExecutionPolicy;
 using Ekid.Infrastructure.Messaging;
+using Ekid.Infrastructure.ModuleContext;
 using Ekid.Infrastructure.Security;
 using Ekid.Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Ekid.Infrastructure;
 
 public static class Bootstrap
 {
-    public static void AddInfrastructure(this IServiceCollection services)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
         services.AddCommandQueryDispatcherWithHandlers();
         services.AddTransactionalHandlers();
         services.AddPermissionContainer();
         services.AddHostedService<AppInitializer>();
+        services.AddAuth(configuration);
+        services.AddSingleton<ExecutionPolicyMiddleware>();
+    }
+
+    public static void UseInfrastructure(this WebApplication app)
+    {
+        app.Services.RegisterAllPermissions();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseMiddleware<ExecutionPolicyMiddleware>();
+        app.UseEndpoints(endpoints => endpoints.UseModuleEndpoints());
+        app.MapControllers();
     }
 }
---
+++ src/Infrastructure/Ekid.Infrastructure/ConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Ekid.Infrastructure;
+
+public static class ConfigurationExtensions
+{
+    public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : class, new()
+    {
+        var options = new T();
+        var section = configuration.GetRequiredSection(sectionName);
+        section.Bind(options);
+
+        return options;
+    }
+}
